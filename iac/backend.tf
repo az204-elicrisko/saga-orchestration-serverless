e1441d40bfd09cce3dd9e6426c0d7c99943000b6
az204-elicrisko <109534368+az204-elicrisko@users.noreply.github.com>
2022-07-20 15:48:49 -0300

    Update backend.tf

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    # modify the values for the storage account 11
+    # modify the values for the storage account 12
     storage_account_name = "funcstorelicrisko"
     container_name       = "testingterraform"
     key                  = "terraform.tfstate"
